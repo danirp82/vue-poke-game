9893145149db2a918a9893026e16c7f1eba7cf9f
Daniel Rodriguez Perez <daniel.rodriguezp@sngular.com>
2021-10-14 12:52:42 +0200

    Add event options component and method random pokemon

--- src/helpers/getOptions.js
+++ src/helpers/getOptions.js
@@ -25,10 +25,10 @@ const getPokemonNames = async([a,b,c,d] = []) => {
     const [p1,p2,p3,p4] = await Promise.all(promiseArr);
 
     return [
-        { name: p1.data.name, id: p1.data.name },
-        { name: p2.data.name, id: p2.data.name },
-        { name: p3.data.name, id: p3.data.name },
-        { name: p4.data.name, id: p4.data.name },
+        { name: p1.data.name, id: p1.data.id },
+        { name: p2.data.name, id: p2.data.id },
+        { name: p3.data.name, id: p3.data.id },
+        { name: p4.data.name, id: p4.data.id },
     ]
 
 }
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,7 +1,22 @@
+
 <template>
-  <h1>¿Quién es este pokemon?</h1>
-  <PokemonPicture :pokemonId="4" :showPokemon="false"/>
-  <PokemonOptions :pokemons="pokemonArr"/>
+    <h1 v-if="!pokemon">Espere por favor...</h1>
+
+    <div v-else>
+        <h1>¿Quién es este pokémon?</h1>
+        
+        <PokemonPicture 
+            :pokemon-id="pokemon.id" 
+            :show-pokemon="showPokemon" 
+        />
+
+        <PokemonOptions 
+            :pokemons="pokemonArr"
+            @selection-pokemon="checkAnswer"
+        />
+
+    </div>
+    
 </template>
 
 <script>
@@ -17,12 +32,20 @@ export default {
   },
   data() {
     return {
-      pokemonArr: []
+      pokemonArr: [],
+      pokemon: null,
+      showPokemon: false,
     }
   },
   methods: {
     async mixPokemonArr() {
       this.pokemonArr = await getOptions()
+
+      const rndInt = Math.floor( Math.random() * 4)
+      this.pokemon = this.pokemonArr[rndInt]
+    },
+    checkAnswer(pokemonId) {
+      this.showPokemon = true
     }
   },
   mounted() {
